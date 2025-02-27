ac550beca1a62518e78cdbe09b5827cb0ca1bb4f
Gustavo Aquino <gustavo.aquino.dev@gmail.com>
2025-02-27 00:31:10 -0300

    Add tailwind css

---
+++ components/ListItem.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="w-full p-4 border border-gray-200 flex items-center justify-between">
+        <span class="text-xl font-semibold">
+            <slot></slot>
+        </span>
+        <div class="w-32 space-x-2">
+            <slot name="actions"></slot>
+        </div>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="w-full px-2">
+        <ListItem>
+            João da Silva
+            <template #actions>
+                <button class="px-2 py-1 bg-green-500 text-white rounded">Editar</button>
+                <button class="px-2 py-1 bg-red-500 text-white rounded">Excluir</button>
+            </template>
+        </ListItem>
+
+        <button class="w-full p-4 mt-8 border border-blue-200 rounded bg-blue-500 text-white flex items-center justify-center">
+            Adicionar Contato
+        </button>
+    </div>
+</template>
+
+<script setup>
+import ListItem from './ListItem.vue';
+</script>
